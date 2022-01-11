aaa126ecfaa6763ec8a8deb2fcb3855b7fd9cd2a
Anne Ho <anniebytes@gmail.com>
2022-01-11 11:33:35 -0800

    Add models for tags

--- model.py
+++ model.py
@@ -21,6 +21,7 @@ class Deckmark(db.Model):
     thumbnail = db.Column(db.String(1000), nullable=True)
 
     group = db.relationship("Group", secondary="group_items", backref="deckmarks")
+    tags = db.relationship("Tag", secondary="decktags", backref="deckmarks")
 
 class User(db.Model):
 
@@ -41,13 +42,36 @@ class Group(db.Model):
     description = db.Column(db.Text, nullable=True)
     private = db.Column(db.Boolean)
 
+    # backref attributes:
+    # .deckmarks - display deckmarks under a group
+
 class groupItem(db.Model):
+    """Association table between groups and deckmarks"""
 
     __tablename__ = "group_items"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     group_id = db.Column(db.Integer, ForeignKey('groups.id'))
     deckmark_id = db.Column(db.Integer, ForeignKey('deckmarks.id'))
+
+class Tag(db.Model):
+
+    __tablename__ = "tags"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = db.Column(db.String(1000), nullable=False)
+
+    # backref attributes:
+    # .deckmarks - display all deckmarks under a tag
+
+class Decktag(db.Model):
+    """Association table between deckmarks and tags"""
+
+    __tablename__ = "decktags"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    deckmark_id = db.Column(db.Integer, ForeignKey('deckmarks.id'))
+    tag_id = db.Column(db.Integer, ForeignKey('tags.id'))
 
 def connect_to_db(app):
     """Connect the database to our Flask app."""
