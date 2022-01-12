99a2edf11991e9552f386e621651e5682b48b56d
Anne Ho <anniebytes@gmail.com>
2022-01-11 16:57:57 -0800

    Add routes for check_user_login, viewing groups and group details, update routes for creating groups and deckmarks with crud functions, add user session validation to routes

--- server.py
+++ server.py
@@ -4,28 +4,39 @@ import crud
 import model
 
 app = Flask(__name__)
+app.secret_key = 'DEV'
 model.connect_to_db(app)
 
 @app.route('/')
-def show_landing_page():
+def view_landing_page():
     return render_template("index.html")
 
-@app.route('/groups')
-def show_all_groups():
-    groups = crud.get_all_groups()
-    return render_template("groups.html", groups=groups)
+@app.route('/flash_test')
+def flash_test():
+    flash('hello')
+    return render_template("flash.html")
 
+# <----- Routes related to User ----->
 @app.route('/login')
 def show_login():
     return render_template("login.html")
 
-@app.route('/process_login')
-def process_login():
+@app.route('/check_user_login', methods=['POST'])
+def check_user_login():
     """Gets data from login form and checks for email/password in db"""
-    return render_template("login.html")
+    email = request.form.get('email')
+    password = request.form.get('password')
+    username = crud.check_user_login(email,password)
+    if username is not False:
+        session['user_logged_in'] = True
+        flash(f'Welcome back {username}')
+        return redirect('/')
+    else:
+        flash('Login unsuccessful', 'error')
+    return redirect('/login')
 
 @app.route('/register')
-def show_registration():
+def view_registration():
     return render_template("add_user_form.html")
 
 @app.route('/create_user', methods=['POST'])
@@ -34,37 +45,60 @@ def create_user():
     lname = request.form.get('lname')
     email = request.form.get('email')
     password = request.form.get('password')
-    crud.create_user_record(fname,lname,email,password)
-
-    # add flash message
-
+    if crud.create_user_record(fname,lname,email,password):
+        flash('new record created', 'message')
+    else:
+        flash('record creation failed', 'error')
     return redirect('/')
+
+# <----- Routes related to Groups ----->
+@app.route('/groups')
+def view_all_groups():
+    groups = crud.get_all_groups()
+    return render_template("groups.html", groups=groups)
+
+
+@app.route('/group/<id>')
+def view_group(id):
+    group = crud.get_deckmarks_by_group_id(id)
+    return render_template("group_details.html", deckmarks=group.deckmarks)
 
 
 @app.route('/add_group')
 def add_group_form():
-    return render_template('add_group_form.html')
+    if session.get('user_logged_in'): 
+        return render_template('add_group_form.html')
+    return redirect('/')
 
 @app.route('/create_group', methods=['POST'])
 def create_group():
     name = request.form.get('name')
     description = request.form.get('description')
     private = request.form.get('private')
-    crud.create_group_record(name, description, private)
+    if crud.create_group_record(name, description, private):
+            flash('new record created', 'message')
+    else:
+        flash('record creation failed', 'error')
     return redirect('/')
 
+# <----- Routes related to Deckmarks ----->
 @app.route('/add_deckmark')
 def add_deckmark_form():
-    return render_template('add_deckmark_form.html')
+    if session.get('user_logged_in'): 
+        return render_template('add_deckmark_form.html')
+    flash('User must be logged in to access this page')
+    return redirect('/')
 
 @app.route('/create_deckmark', methods=['POST'])
 def create_deckmark():
     link = request.form.get('link')
     description = request.form.get('description')
     thumbnail = request.form.get('thumbnail')
-    crud.create_deckmark_record(link, description, thumbnail)
+    if crud.create_deckmark_record(link, description, thumbnail):
+            flash('new record created', 'message')
+    else:
+        flash('record creation failed', 'error')
     return redirect('/')
-
 
 
 if __name__ == "__main__":
