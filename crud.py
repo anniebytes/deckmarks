04bd7fa5bcb99b331e9844dc173a2c1dcc45c4d9
Anne Ho <anniebytes@gmail.com>
2022-01-11 16:47:11 -0800

    Add functions for check_user_login and add_deckmarks_by_group_id

--- crud.py
+++ crud.py
@@ -6,16 +6,25 @@ def create_user_record(fname, lname, email, password):
     user = model.User(fname=fname, lname=lname, email=email, password=password)
     model.db.session.add(user)
     model.db.session.commit()
+    return True
+
+def check_user_login(email, password):
+    user = model.User.query.filter(model.User.email == email).one()
+    if user and user.password == password:
+        return user.fname
+    return False
 
 def create_deckmark_record(link, description, thumbnail):
     deckmark = model.Deckmark(link=link, description=description, thumbnail=thumbnail)
     model.db.session.add(deckmark)
     model.db.session.commit()
+    return True
 
 def create_group_record(name, description,private=False):
     group = model.Group(name=name, description=description,private=private)
     model.db.session.add(group)
     model.db.session.commit()
+    return True
 
 def get_group_id(name):
     """Returns group object"""
@@ -24,6 +33,11 @@ def get_group_id(name):
 def get_all_groups():
     """Returns list of group objects"""
     return model.Group.query.all()
+
+def get_deckmarks_by_group_id(group_id):
+    """Returns group object"""
+    # add validation that only one group is returned
+    return model.Group.query.filter(model.Group.id == group_id).one()
 
 def add_deckmark_to_group(group_id, deckmark_id):
     """Creates association between group and deckmark"""
