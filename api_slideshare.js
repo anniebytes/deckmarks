a57b38aadadce58781c19064b78a099e68457fed
Annie Ho <anniebytes@gmail.com>
2022-01-13 23:37:09 -0800

    Refactor url string to use URLSearchParams

--- api_slideshare.js
+++ api_slideshare.js
@@ -10,15 +10,22 @@ const timestamp = Math.round((new Date().getTime())/1000);
 const hash = crypto.createHash('sha1')
                     .update(SHARED_SECRET + timestamp)
                     .digest('hex');
-const url = "https://www.slideshare.net/api/2/get_slideshows_by_tag";
-const tag = "technology";
-const url_string = `${url}?tag=${tag}&limit=1&api_key=${API_KEY}&hash=${hash}&ts=${timestamp}`;
 
-console.log(url_string);
+const url = "https://www.slideshare.net/api/2/get_slideshows_by_tag";
+
+const queryString = new URLSearchParams({
+        tag: "technology",
+        limit: 1,
+        api_key: API_KEY,
+        hash: hash,
+        ts: timestamp,
+    }).toString();
+
+const urlString = `${url}?${queryString}`;
 
 // TODO: look into async/await solution
 let result;
-fetch(url_string)
+fetch(urlString)
     .then(response => response.text())
     .then(data => {
         result = data;
