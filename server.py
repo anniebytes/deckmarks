46132f9c8831731f846bfd1ae3227197c1c2c8c1
Annie Ho <anniebytes@gmail.com>
2022-01-23 20:55:20 -0800

    Add routes for adding and creating tags

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, session, flash, redirect
+from flask import Flask, request, session, flash, redirect, jsonify
 from flask.templating import render_template
 import crud
 import model
@@ -65,7 +65,6 @@ def view_group(id):
     group = crud.get_deckmarks_by_group_id(id)
     return render_template("group_details.html", group=group)
 
-
 @app.route('/add_group')
 def add_group_form():
     if session.get('user_id'): 
@@ -110,9 +109,14 @@ def create_deckmark():
     if not user_id:
         flash('Must be a logged in user to create a group.')
         return redirect('/add_deckmark')
-    link = request.form.get('link')
-    description = request.form.get('description')
-    thumbnail = request.form.get('thumbnail')
+    if request.form.get('link'):
+        link = request.form.get('link')
+        description = request.form.get('description')
+        thumbnail = request.form.get('thumbnail')
+    if request.json.get('link'):
+        link = request.json.get('link')
+        description = request.json.get('description')
+        thumbnail = request.json.get('thumbnail')
     deckmark = crud.create_deckmark_record(
                                 user_id=user_id,
                                 link=link,
@@ -132,6 +136,11 @@ def create_deckmark():
         flash('record creation failed', 'error')
     return redirect("/groups")
 
+@app.route('/deckmark/<id>/tags')
+def view_deckmark_tags(id):
+    json_dict = crud.get_tags_by_deckmark_id(id)
+    return jsonify(json_dict)
+
 @app.route('/browse/slideshare/<tag>')
 def slideshare_api(tag):
     response_text = ss_api.api_request(tag)
@@ -143,6 +152,24 @@ def slideshare_api(tag):
         slideshow_list = parse_xml.slideshow_list(response_text)
         groups = crud.get_all_groups()
         return render_template('response.html', slideshow_list=slideshow_list, groups=groups)
+
+@app.route('/api/add_tag_to_deckmark', methods=["POST"])
+def add_tag_to_deckmark():
+    deckmark_id = request.json.get('deckmark_id')
+    tag_id = request.json.get('tag_id')
+    if crud.add_tag_to_deckmark(deckmark_id, tag_id):
+        return {"success": True}
+    else:
+        return {"success": False}
+
+@app.route('/api/create_tag', methods=["POST"])
+def create_tag():
+    tag_name = request.json.get('name')
+    tag_id = crud.create_tag(tag_name)
+    if tag_id:
+        return {"tag_id": tag_id}
+    else:
+        return {"success": False}
 
 if __name__ == "__main__":
     app.run(debug=True)
