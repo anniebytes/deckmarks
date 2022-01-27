326e0e15940aad4e6a13fa2775f51f40ede0c204
Annie Ho <anniebytes@gmail.com>
2022-01-27 11:46:16 -0800

    Add additional links to browse Slideshare API by different categories

--- static/jsx/index.jsx
+++ static/jsx/index.jsx
@@ -35,9 +35,33 @@ function SlideshareLinks(){
     return(
         <div>
             <RouteLink route="browse/slideshare/technology" 
-                        linkTitle="Browse Slideshares By Technology"/><br></br>
+                        linkTitle="Browse Slideshares On Technology"/><br></br>
             <RouteLink route="browse/slideshare/lifestyle" 
-                        linkTitle="Browse Slideshares By Lifestyle"/><br></br>    
+                        linkTitle="Browse Slideshares On Lifestyle"/><br></br>    
+            <RouteLink route="browse/slideshare/art-photos" 
+                        linkTitle="Browse Slideshares On Art"/><br></br>    
+            <RouteLink route="browse/slideshare/design" 
+                        linkTitle="Browse Slideshares On Design"/><br></br>    
+            <RouteLink route="browse/slideshare/business" 
+                        linkTitle="Browse Slideshares On Business"/><br></br>    
+            <RouteLink route="browse/slideshare/career" 
+                        linkTitle="Browse Slideshares On Career"/><br></br>    
+            <RouteLink route="browse/slideshare/economy-finance" 
+                        linkTitle="Browse Slideshares On Finance"/><br></br>
+            <RouteLink route="browse/slideshare/food" 
+                        linkTitle="Browse Slideshares On Food"/><br></br>    
+            <RouteLink route="browse/slideshare/data-analytics" 
+                        linkTitle="Browse Slideshares On Data"/><br></br> 
+            <RouteLink route="browse/slideshare/entertainment-humor" 
+                        linkTitle="Browse Slideshares On Entertainment"/><br></br> 
+            <RouteLink route="browse/slideshare/engineering" 
+                        linkTitle="Browse Slideshares On Engineering"/><br></br> 
+            <RouteLink route="browse/slideshare/devices-hardware" 
+                        linkTitle="Browse Slideshares On Hardware"/><br></br> 
+            <RouteLink route="browse/slideshare/education" 
+                        linkTitle="Browse Slideshares On Education"/><br></br> 
+            <RouteLink route="browse/slideshare/environment" 
+                        linkTitle="Browse Slideshares On Environment"/><br></br> 
         </div>
     )
 }
