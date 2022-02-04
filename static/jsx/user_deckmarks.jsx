165565f5cd6f8f2813915ebbf0ace237c069656d
Annie Ho <anniebytes@gmail.com>
2022-02-04 12:02:00 -0800

    Add react components to view user-owned deckmarks

---
+++ static/jsx/user_deckmarks.jsx
@@ -0,0 +1,98 @@
+const Thumbnail = (props) => {
+
+    return (
+        <div>
+            <img src="{props.image}"></img>
+        </div>
+    )
+}
+
+
+const Link = (props) => {
+
+    return (
+        <div>
+            <a href="{props.link}">{props.title}</a>
+        </div>
+    )
+}
+
+
+const Description = (props) => {
+
+    return (
+        <div>
+            <p>
+                {props.description}
+            </p>
+        </div>
+    )
+}
+
+
+const UserInfo = (props) => {
+
+    return (
+        <div>
+            {props.id}
+        </div>
+    )
+}
+
+
+const Deckmark = (props) => {
+
+    return (
+        <div>
+            <Thumbnail image={props.image}/>
+            <Link link={props.link} title={props.link}/>
+            <Description description={props.description}/>
+            <UserInfo id={props.user_id}/>
+        </div>
+    )
+}
+
+
+const UserDeckmarks = (props) => {
+    console.log(props.user_id)
+    const [deckmarks, setDeckmarks] = React.useState([]);
+    React.useEffect(() => {
+      fetch(`/api/user/${props.user_id}/deckmarks`)
+        .then(response => response.json())
+        .then(result => {
+          setDeckmarks(result);
+        });
+    }, []);
+
+    if (deckmarks.length === 0) {
+        return <p>Loading...</p>;
+    }
+
+    const deckmarkItems = deckmarks.map((deckmark) => {
+        return (
+            <div key={deckmark.id}>
+                <Deckmark 
+                    image={deckmark.thumbnail}
+                    link={deckmark.link}
+                    description={deckmark.description}
+                    user_id={deckmark.user_id}
+                />
+            </div>);
+
+    });
+
+    return (
+        <div>
+            {deckmarkItems}
+        </div>
+    )
+
+}
+
+const user_div = document.querySelector('.user')
+const user_id = user_div.id.split('-')[1]
+
+ReactDOM.render(
+    <UserDeckmarks user_id={user_id} />,
+    document.querySelector('.user')
+)
