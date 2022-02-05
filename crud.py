befcc35b063dce68a0354d54c1fa753965b015ec
Annie Ho <anniebytes@gmail.com>
2022-02-05 14:00:11 -0800

    Add functions for getting tags from database

--- crud.py
+++ crud.py
@@ -8,11 +8,13 @@ def create_user_record(fname, lname, email, password):
     model.db.session.commit()
     return True
 
+
 def check_user_login(email, password):
     user = model.User.query.filter(model.User.email == email).one()
     if user and user.password == password:
         return user
     return None
+
 
 def create_deckmark_record(user_id, link, description, thumbnail):
     deckmark = model.Deckmark(user_id=user_id, link=link, description=description, thumbnail=thumbnail)
@@ -20,11 +22,13 @@ def create_deckmark_record(user_id, link, description, thumbnail):
     model.db.session.commit()
     return deckmark
 
+
 def create_group_record(user_id, name, description,private=False):
     group = model.Group(user_id=user_id, name=name, description=description,private=private)
     model.db.session.add(group)
     model.db.session.commit()
     return True
+
 
 def update_group_name(id, name, new_name):
     """Returns group object"""
@@ -78,19 +82,29 @@ def add_deckmark_to_group(group_id, deckmark_id):
     model.db.session.commit()
     return group_item
 
+
+def get_tag_name_by_id(tag_id):
+    return model.Tag.query.filter(model.Tag.id == tag_id).one().name
+
+
+def get_tag_id_by_name(tag_name):
+    tag = model.Tag.query.filter(model.Tag.name == tag_name).first()
+    if not tag:
+        return False
+    return tag.id
+
+
 def get_tags_by_deckmark_id(deckmark_id):
-    """Returns JSON string of tags"""
-    tags_list = model.Deckmark.query.filter(model.Deckmark.id == deckmark_id).one().tags
-    json_dict = { "tags" : "" }
-    for tag in tags_list:
-        json_dict["tags"] += tag.name
-    return json_dict
+    """Returns list of Tag database objects"""
+    return model.Deckmark.query.filter(model.Deckmark.id == deckmark_id).one().tags
+
 
 def create_tag(name):
     tag = model.Tag(name=name)
     model.db.session.add(tag)
     model.db.session.commit()
     return tag.id
+
 
 def add_tag_to_deckmark(deckmark_id, tag_id):
     """Creates association between deckmark and tag"""
@@ -100,6 +114,7 @@ def add_tag_to_deckmark(deckmark_id, tag_id):
     if tag:
         return True
     return None
+
 
 if __name__ == "__main__":
     from server import app
